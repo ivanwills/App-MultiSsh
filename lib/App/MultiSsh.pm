861e02978d767f30ecc048e37f4adba5fd021229
Ivan Wills <Ivan.Wills2@optus.com.au>
2015-06-01 11:03:58 +1000

    Updating for release of version 0.11

--- lib/App/MultiSsh.pm
+++ lib/App/MultiSsh.pm
@@ -14,7 +14,7 @@ use Data::Dumper qw/Dumper/;
 use English qw/ -no_match_vars /;
 use base qw/Exporter/;
 
-our $VERSION     = '0.10';
+our $VERSION     = '0.11';
 our @EXPORT_OK   = qw/hosts_from_map is_host multi_run shell_quote tmux/;
 our %EXPORT_TAGS = ();
 
@@ -182,7 +182,7 @@ App::MultiSsh - Multi host ssh executer
 
 =head1 VERSION
 
-This documentation refers to App::MultiSsh version 0.10
+This documentation refers to App::MultiSsh version 0.11
 
 =head1 SYNOPSIS
 
